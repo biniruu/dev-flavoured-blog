91df70dec995c8fcefc4a7c1d0c51abff292df55
biniruu <biniruu@gmail.com>
2023-08-14 16:09:03 +0900

    Updates

--- component---src-pages-404-tsx-7b52d85042b33738f78b.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdev_flavoured_blog=self.webpackChunkdev_flavoured_blog||[]).push([[218],{2513:function(e,l,n){n.r(l);var t=n(7294),u=n(7373);l.default=e=>t.createElement(u.Z,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-7b52d85042b33738f78b.js.map
